d2008093c9d87ad520406aff7a609839e1e384b7
timriffe <tim.riffe@gmail.com>
2022-08-25 09:52:01 +0200

    adds Oslo Norway

--- R/Estonia.R
+++ R/Estonia.R
@@ -1,11 +1,22 @@
 # Harju County has Tallin
 
-EE_url <- "https://opendata.digilugu.ee/opendata_covid19_test_location.csv"
+EE_url <- "https://opendata.digilugu.ee/opendata_covid19_test_county_all.csv"
 download.file(EE_url, destfile = "Data/EE_orig.csv")
 
-library("tidyverse")
+library(tidyverse)
 EE <- read_csv("Data/EE_orig.csv")
+all_dates <- seq(min(EE$StatisticsDate),max(EE$StatisticsDate),by="days")
 EE %>% 
-  filter(Commune == "Tallin") %>% 
+  filter(County == "Harju maakond",
+         ResultValue == "P") %>% 
   mutate(city = "Tallinn",
-         country = "Estonia") 
+         country = "Estonia") %>% 
+  arrange(StatisticsDate) %>% 
+    rename(date = StatisticsDate) %>% 
+  select(date,city,country,DailyCases) %>% 
+  complete(date = all_dates, 
+           city, 
+           country,
+           fill = list(DailyCases = 0)) %>% 
+  write_csv("data_output/estonia.csv")
+
--- R/Norway.R
+++ R/Norway.R
@@ -1,16 +1,39 @@
-library(lubridate)
-library(tidyverse)
-library(readr)
-library(readxl)
-rm (list = ls())
+# library(lubridate)
+# library(tidyverse)
+# library(readr)
+# library(readxl)
+# rm (list = ls())
+# 
+# # db <- read_csv("https://raw.githubusercontent.com/folkehelseinstituttet/surveillance_data/master/covid19/data_covid19_msis_by_time_location_latest.csv")
+# # write_csv(db, "data_input/norway.csv")
+# 
+# db <- read_csv("data_input/norway.csv")
+# 
+# db2 <- 
+#   db %>% 
+#   filter(location_name == "Oslo") %>% 
+#   select(city = location_name, date, cases = n) %>% 
+#   mutate(country = "Norway")
 
-# db <- read_csv("https://raw.githubusercontent.com/folkehelseinstituttet/surveillance_data/master/covid19/data_covid19_msis_by_time_location_latest.csv")
-# write_csv(db, "data_input/norway.csv")
+# TR redux
+NO_url <- "https://raw.githubusercontent.com/thohan88/covid19-nor-data/master/data/01_infected/msis/municipality.csv"
 
-db <- read_csv("data_input/norway.csv")
+download.file(NO_url, destfile = "Data/NO_orig.csv")
+NO <- read_csv("Data/NO_orig.csv") 
 
-db2 <- 
-  db %>% 
-  filter(location_name == "Oslo") %>% 
-  select(city = location_name, date, cases = n) %>% 
-  mutate(country = "Norway")
+
+all_dates <- seq(min(NO$date),max(NO$date),by="days")
+NO %>% 
+  filter(kommune_name == "Oslo") %>% 
+  mutate(width = as.integer(lead(date) - date),
+         cases = lead(cases)-cases) %>% 
+  complete(date = all_dates, kommune_name) %>% 
+  arrange(date) %>% 
+  fill(width, cases) %>% 
+  mutate(cases = cases / width,
+         country = "Norway") %>% 
+  select(city = kommune_name,
+         date,
+         cases,
+         country) %>% 
+  write_csv("data_output/norway.csv")
